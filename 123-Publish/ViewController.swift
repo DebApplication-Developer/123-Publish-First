cbd5a63f85a633639162d852a19eaa9a246b943b
coderpath <palashiosdeveloper@gmail.com>
2024-10-04 17:05:20 +0530

    Initial Commit

---
+++ 123-Publish/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  123-Publish
+//
+//  Created by CoderPath on 04/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var show: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        show.text = "iiooppuu"
+    }
+
+
+}
+
